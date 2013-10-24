139d96b27be06e0de72824a4c9954d8ef858e6b7
Brad Nelson <bradnelson@google.com>
2013-10-24 13:49:48 -0700

    fonts show up

--- font.fs
+++ font.fs
@@ -1,16 +1,30 @@
 ( ------------------------------------------------------------ )
 
-variable weight  5 weight !
+: square ( n -- n ) dup * ;
+: sqrt ( n -- n ) s>f fsqrt f>s ;
+
+8 constant granularity
+1 granularity lshift constant granual
+granual square constant granual2
+
+variable weight  6 granularity lshift weight !
+
 
 : clear ( -- ) 0 0 pixel width height * 4 * 255 fill ;
-: plot ( x y -- ) pixel 0 over c! 0 over 1+ c! 0 swap 2 + c! ;
+: byte-clip ( n -- n ) 0 max 255 min ;
+: pattern ( x y -- )
+  square swap square + sqrt weight @ granual - - byte-clip ;
+: blend1 ( v a -- ) dup c@ rot min swap c! ;
+: blend ( v a -- ) 2dup blend1 2dup 1+ blend1 2 + blend1 ;
 : penplot ( x y -- )
-  dup weight @ + swap weight @ - do
-    dup weight @ + over weight @ - do
-      i j plot
-    loop
-  loop
-  drop
+  weight @ 1+ weight @ negate do
+    weight @ 1+ weight @ negate do
+      2dup j + granularity rshift swap
+           i + granularity rshift swap pixel
+      i j pattern swap blend
+    granual +loop
+  granual +loop
+  2drop
 ;
 
 ( ------------------------------------------------------------ )
@@ -18,15 +32,14 @@ variable weight  5 weight !
 : p! ( x y a -- ) swap over cell+ ! ! ;
 : p@ ( a -- x y ) dup @ swap cell+ @ ;
 
-: square ( n -- n ) dup * ;
 : distance2 ( x1 y1 x2 y2 ) rot - square -rot - square + ;
 : middle ( x1 y1 x2 y2 -- mx my ) rot + 2/ >r + 2/ r> ;
 
 point q1  point q2  point q3  point q12  point q23  point q123
 
-: quartic ( p1 p2 p3 -- )
+: quartic' ( p1 p2 p3 -- )
   q3 p! q2 p! q1 p!
-  q1 p@ q3 p@ distance2 10 < if
+  q1 p@ q3 p@ distance2 granual2 2/ < if
     q1 p@ q3 p@ middle penplot
   else
     q1 p@ q2 p@ middle q12 p!
@@ -37,10 +50,35 @@ point q1  point q2  point q3  point q12  point q23  point q123
     recurse recurse
   then 
 ;
+: quartic ( p1 p2 p3 -- )
+  granularity lshift >r granularity lshift >r granularity lshift >r
+  granularity lshift >r granularity lshift >r granularity lshift >r 
+  r> r> r> r> r> r>
+  quartic'
+;
 
 ( ------------------------------------------------------------ )
 ( Font definition tools )
-: /;,   99 c, ;
-: /,   dup 10000 / c,  dup 100 / c,  c, ;
-
-
+: /;,   here 3 - dup c@ 128 or swap c! ;
+: /,   dup 10000 / 100 mod c,
+       dup 100 / 100 mod c,
+       100 mod c, ;
+variable font
+variable font-x
+variable font-y
+33 constant min-character  126 constant max-character
+: char-clip ( n -- n ) min-character max max-character min ;
+: char-offset ( n -- n ) char-clip min-character - ;
+: char-end? ( n -- n ) 128 and 0<> ;
+: char-skip ( a -- a ) begin dup 3 + swap c@ char-end? until ;
+: char-start ( n -- a ) char-offset font @ swap 0 ?do char-skip loop ;
+: stroke-code ( n -- c ) 127 and ;
+: stroke-x ( n -- x ) 10 /   2 - 30 * 100 + font-x @ + ;
+: stroke-y ( n -- y ) 10 mod 2 - 32 * negate 400 + font-y @ + ;
+: stroke-pt ( n -- x y ) stroke-code dup stroke-x swap stroke-y ;
+: stroke-draw ( a -- )
+  dup c@ stroke-pt rot dup 1+ c@ stroke-pt rot 2 + c@ stroke-pt
+  quartic ;
+: char-draw ( n -- ) char-start begin dup c@ char-end? 0= while
+                       dup stroke-draw 3 + repeat stroke-draw
+                       100 font-x +! ;
--- test1.fs
+++ test1.fs
@@ -3,37 +3,32 @@ s" font.fs" included
 s" gimple1.fs" included
 ( ------------------------------------------------------------ )
 
-: draw
-  0 0 pixel
-  height 0 do
-    width 0 do
-      i 255 height 1- */ over 0 + c!
-      i 255 height 1- */ over 1 + c!
-      i 255 height 1- */ over 2 + c!
-      4 +
-    loop
-  loop
-  drop
-;
+variable x  33 x !
 
 : test1
   640 480 window
   begin
     poll
     event expose-event = if
-      \ draw
       clear
-      100 100 300 100 300 300 quartic
-      100 100 100 300 300 300 quartic
+\      100 100 300 100 300 300 quartic
+\      100 100 100 300 300 300 quartic
       flip
     then
     event 0= if
       \ move mouse
     else event timeout-event = if
       100 ms
+    else event 1 = if
+      clear
+      gimple1 font !
+      0 font-x !
+      x @ dup 5 + swap do i char-draw loop
+      flip
+      1 x +!
     else
-      event . mouse-x . mouse-y . last-keysym . last-key emit cr
-    then then
+      \ event . mouse-x . mouse-y . last-keysym . last-key emit cr
+    then then then
   again
 ;
 test1
